3edc7581c5df817c8f8b14520f7a49be285b06fe
Ubuntu <ubuntu@ip-172-31-42-14.ec2.internal>
2025-03-01 18:58:37 +0000

    update the  changes

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "output_name" {
-    value = "some resource"
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
 }
